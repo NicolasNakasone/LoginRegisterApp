655759bc625dfd5cc1c31a9e479795fb7baf08b0
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-11-05 19:31:54 -0300

    feat(database): connected with postgresql database

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,11 @@
 import server from 'src/app'
+import { sequelize } from 'src/database'
 
 const API_PORT: string | undefined = server.get('port')
 
-server.listen(API_PORT, () => {
-  console.log(`Listening at port ${API_PORT}!`)
+sequelize.sync({ force: false }).then(() => {
+  console.log('Database connected successfully!')
+  server.listen(API_PORT, () => {
+    console.log(`Listening at port ${API_PORT}!`)
+  })
 })
