0cd9f211725ebb20f72d62895bc366bdcd6b980f
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-11-06 23:20:45 -0300

    fix(config): modified max number of lines on .prettierrc

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   tabWidth: 2,
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
   singleQuote: true,
   semi: false,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   bracketSpacing: true,
-  endOfLine: "lf",
-};
+  endOfLine: 'lf',
+  // Por algún motivo, al setear printWidth: 100, en realidad te permite 101 lineas de largo
+  printWidth: 99,
+}
