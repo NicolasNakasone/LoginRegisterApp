f2539d4fea76b466c11c901b4d9bba6f8d91a462
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-05-03 00:30:05 -0300

    refactor(styles): modified some styles

--- api/src/routes/login.ts
+++ api/src/routes/login.ts
@@ -6,4 +6,10 @@ export const loginRouter = express.Router()
 loginRouter.get('/', (req, res) => {
   res.send(`<h1>LoginRegisterApp - ${routes.login}<h1>`)
 })
-loginRouter.post('/', () => null)
+loginRouter.post('/', (req, res, next) => {
+  try {
+    console.log(req)
+  } catch (error) {
+    next(error)
+  }
+})
--- client/src/pages/register/Register.page.tsx
+++ client/src/pages/register/Register.page.tsx
@@ -1,10 +1,22 @@
 export const RegisterPage = (): JSX.Element => {
   return (
-    <form>
-      <input type="text" placeholder="Nombre y apellido" />
-      <input type="email" placeholder="Correo" />
-      <input type="password" placeholder="Contraseña" />
-      <input type="password" placeholder="Repetir contraseña" />
-    </form>
+    <main
+      className="w-[60vw] h-[60vh] m-auto p-4 flex items-center gap-16 border-2 border-[#FFFFFF40] rounded-xl overflow-hidden"
+      style={{
+        backgroundColor: '#FFFFFF20',
+        backdropFilter: 'blur(4px)',
+        boxShadow: '0 0 12px 4px #00000040',
+      }}
+    >
+      <section className="mx-8 flex place-items-center">
+        <p>LoginRegisterApp</p>
+      </section>
+      <form className="flex flex-col justify-center gap-8">
+        <input type="text" placeholder="Nombre y apellido" />
+        <input type="email" placeholder="Correo" />
+        <input type="password" placeholder="Contraseña" />
+        <input type="password" placeholder="Repetir contraseña" />
+      </form>
+    </main>
   )
 }
