96ebd064a3988e4c571833f36977ad0c27cab056
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-05-23 22:50:58 -0300

    refactor(app): modified app logic to be more simple

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react'
+
 import { Route, Routes } from 'react-router-dom'
 import { routes } from 'src/constants/routes'
 import { HomePage, LoginPage, RegisterPage } from 'src/pages'
 
 export const App = (): JSX.Element => {
+  const [isLogged, setIsLogged] = useState(false)
+
+  if (!isLogged) return <h1>Debes iniciar sesion</h1>
+
   return (
-    <Routes>
-      <Route path={routes.home} element={<HomePage />} />
-      <Route path={routes.login} element={<LoginPage />} />
-      <Route path={routes.register} element={<RegisterPage />} />
-    </Routes>
+    <h1>Bienvenido</h1>
+    // <Routes>
+    //   <Route path={routes.home} element={<HomePage />} />
+    //   <Route path={routes.login} element={<LoginPage />} />
+    //   <Route path={routes.register} element={<RegisterPage />} />
+    // </Routes>
   )
 }
