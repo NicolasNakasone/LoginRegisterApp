c536cf9e04ed0afe7681f48105b0f89438c4047c
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-05-25 23:43:38 -0300

    feat(login): initial version of login
    * This only check the equality of email and password from user input with the stored ones on mockUsers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { SetStateAction, useState } from 'react'
 
-import { Route, Routes } from 'react-router-dom'
-import { routes } from 'src/constants/routes'
-import { HomePage, LoginPage, RegisterPage } from 'src/pages'
+// import { Route, Routes } from 'react-router-dom'
+// import { routes } from 'src/constants/routes'
+import { HomePage, LoginPage /* , RegisterPage */ } from 'src/pages'
+
+export interface CommonPageProps {
+  setIsLogged: (value: SetStateAction<boolean>) => void
+}
 
 export const App = (): JSX.Element => {
   const [isLogged, setIsLogged] = useState(false)
 
-  if (!isLogged) return <LoginPage />
+  if (!isLogged) return <LoginPage {...{ setIsLogged }} />
 
   return (
     <HomePage />
--- client/src/pages/login/Login.page.tsx
+++ client/src/pages/login/Login.page.tsx
@@ -1,27 +1,86 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
-const mockUsers = [
-  {
+import { CommonPageProps } from 'src/App'
+
+interface LoginFormValues {
+  email: string
+  password: string
+}
+
+const mockUsers: Record<string, { id: string; email: string; password: string } | undefined> = {
+  ['foo@bar.com']: {
     id: `${+new Date()}`,
     email: 'foo@bar.com',
     password: '1234',
   },
-]
+}
 
-export const LoginPage = (): JSX.Element => {
+export const LoginPage = ({ setIsLogged }: CommonPageProps): JSX.Element => {
+  const [formValues, setFormValues] = useState<LoginFormValues>({
+    email: '',
+    password: '',
+  })
+
   const [isPassword, setIsPassword] = useState(true)
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    setIsLoading(true)
+    e.preventDefault()
+
+    const { email, password } = formValues
+
+    const isUserRegistered = mockUsers[email]
+
+    if (!isUserRegistered) {
+      // TODO: navegar al registro
+      setIsLoading(false)
+      return
+    }
+
+    const isSameEmail = isUserRegistered?.email === email
+    const isSamePassword = isUserRegistered?.password === password
+
+    if (!(isSameEmail && isSamePassword)) {
+      setFormValues({ email: '', password: '' })
+      setIsLoading(false)
+      return
+    }
+
+    // TODO: navegar al home
+    setIsLoading(false)
+    setIsLogged(true)
+    return
+  }
 
   return (
     <>
       <h1>Inicia sesión para poder ver la app</h1>
-      <form>
-        <input type="text" placeholder="Correo" />
+      <form onSubmit={e => handleLogin(e)}>
+        <input
+          name="email"
+          value={formValues.email}
+          type="text"
+          placeholder="Correo"
+          onChange={e => setFormValues(prevValues => ({ ...prevValues, email: e.target.value }))}
+        />
         {/* TODO: Wrappear input y button en un solo componente */}
-        <input type={isPassword ? 'password' : 'text'} placeholder="Contraseña" />
+        <input
+          name="password"
+          value={formValues.password}
+          type={isPassword ? 'password' : 'text'}
+          placeholder="Contraseña"
+          onChange={e =>
+            setFormValues(prevValues => ({ ...prevValues, password: e.target.value }))
+          }
+        />
         <button type="button" onClick={() => setIsPassword(!isPassword)}>
           {isPassword ? `Mostrar 🧐` : `Ocultar 😴`}
         </button>
-        <button type="submit">Iniciar sesión</button>
+        <button disabled={isLoading} type="submit">
+          Iniciar sesión
+        </button>
       </form>
     </>
   )
