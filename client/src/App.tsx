585938f6e930da5f82398b0d575f6879e35aee42
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-05-27 14:37:01 -0300

    feat(property): added full_name to User type

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,21 +12,24 @@ export interface User {
   id: string
   email: string
   password: string
+  full_name: string
 }
 
 // Mover a api.ts
-type UserList = Record<string, { id: string; email: string; password: string }>
+type UserList = Record<string, User>
 
 const mockUsers: UserList = {
   ['foo@bar.com']: {
     id: `${+new Date()}`,
     email: 'foo@bar.com',
     password: '1234',
+    full_name: 'foo@bar.com',
   },
   ['asd@asd.com']: {
     id: `${+new Date()}`,
     email: 'asd@asd.com',
     password: '1234',
+    full_name: 'asd@asd.com',
   },
 }
 
