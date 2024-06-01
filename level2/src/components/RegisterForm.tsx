a7b6760939a570efb46a6ee372b8adb9ec1b43bd
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-01 15:13:50 -0300

    refactor(state): removed state formValues from register form

--- level2/src/components/RegisterForm.tsx
+++ level2/src/components/RegisterForm.tsx
@@ -1,26 +1,13 @@
-import { ChangeEvent, FormEvent, memo, useContext, useState } from 'react'
+import { FormEvent, memo, useContext } from 'react'
 
 import { Link } from 'react-router-dom'
-import { UserInput, api } from 'src/api'
+import { api } from 'src/api'
 import { routes } from 'src/constants/routes'
 import { UserContext } from 'src/contexts/UserContext'
 import { useFormStatus } from 'src/hooks/useFormStatus.hook'
 
-interface RegisterFormValues extends UserInput {
-  re_password: string
-}
-
-const initialFormValues: RegisterFormValues = {
-  full_name: '',
-  email: '',
-  password: '',
-  re_password: '',
-}
-
 export const RegisterForm = (): JSX.Element => {
   const { navigate } = useContext(UserContext)
-
-  const [formValues, setFormValues] = useState<RegisterFormValues>(initialFormValues)
 
   const { error, isLoading, setError, setIsLoading } = useFormStatus()
 
@@ -29,11 +16,19 @@ export const RegisterForm = (): JSX.Element => {
     setIsLoading(true)
     e.preventDefault()
 
-    const { email, full_name, password, re_password } = formValues
+    const target = e.target as HTMLFormElement
 
-    if (await api.findUser(email)) {
+    const full_name = target[0] as HTMLInputElement
+    const email = target[1] as HTMLInputElement
+    const password = target[2] as HTMLInputElement
+    const re_password = target[3] as HTMLInputElement
+
+    if (await api.findUser(email.value)) {
+      full_name.value = ''
+      email.value = ''
+      password.value = ''
+      re_password.value = ''
       setError('❌ Usuario ya registrado')
-      setFormValues(initialFormValues)
       setIsLoading(false)
       return
     }
@@ -45,54 +40,27 @@ export const RegisterForm = (): JSX.Element => {
       Y en handleRegister hacer un solo setIsLoading en false por ej
       No se, capaz es mas quilombo...
     */
-    if (password !== re_password) {
+    if (password.value !== re_password.value) {
+      password.value = ''
+      re_password.value = ''
       setError('❌ Contraseñas no coinciden')
-      setFormValues(prevValues => ({ ...prevValues, password: '', re_password: '' }))
       setIsLoading(false)
       return
     }
 
-    await api.addUser({ full_name, email, password })
+    await api.addUser({ full_name: full_name.value, email: email.value, password: password.value })
 
     navigate(routes.login)
     setIsLoading(false)
     return
   }
 
-  const handleChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
-    setFormValues(prevValues => ({ ...prevValues, [e.target.name]: e.target.value }))
-  }
-
   return (
     <form onSubmit={handleRegister}>
-      <input
-        name={'full_name'}
-        value={formValues.full_name}
-        type="text"
-        placeholder="Nombre y apellido"
-        onChange={handleChangeValue}
-      />
-      <input
-        name={'email'}
-        value={formValues.email}
-        type="email"
-        placeholder="Correo"
-        onChange={handleChangeValue}
-      />
-      <input
-        name={'password'}
-        value={formValues.password}
-        type="password"
-        placeholder="Contraseña"
-        onChange={handleChangeValue}
-      />
-      <input
-        name={'re_password'}
-        value={formValues.re_password}
-        type="password"
-        placeholder="Repetir contraseña"
-        onChange={handleChangeValue}
-      />
+      <input name={'full_name'} type="text" placeholder="Nombre y apellido" />
+      <input name={'email'} type="email" placeholder="Correo" />
+      <input name={'password'} type="password" placeholder="Contraseña" />
+      <input name={'re_password'} type="password" placeholder="Repetir contraseña" />
       <button disabled={isLoading} type="submit">
         Registrate
       </button>
