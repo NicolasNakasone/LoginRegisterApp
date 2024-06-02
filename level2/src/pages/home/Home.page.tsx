72558e91a552c374f7551dcf670a7b79d23fad17
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-02 14:17:23 -0300

    feat(storage): get logged user from localStorage
    * Also, it has been added an early return with a simple text when user is not setted
    
    * It has been added a minimum fix to the css. Now the p selector points to all "p"s wrapped in a form tag

--- level2/src/pages/home/Home.page.tsx
+++ level2/src/pages/home/Home.page.tsx
@@ -1,29 +1,50 @@
 import { useContext, useEffect } from 'react'
 
+import { User } from 'src/api'
 import { routes } from 'src/constants/routes'
 import { UserContext } from 'src/contexts/UserContext'
 import { useFormStatus } from 'src/hooks/useFormStatus.hook'
 
 export const HomePage = (): JSX.Element => {
-  const { isLogged, setIsLogged, user, navigate } = useContext(UserContext)
+  const { setIsLogged, user, setUser, navigate } = useContext(UserContext)
 
   const { isLoading, setIsLoading } = useFormStatus()
 
+  const asyncSetUser = async () => {
+    if (user) return
+
+    let foundUser = localStorage.getItem('user')
+    if (!foundUser) {
+      navigate(routes.login)
+      return
+    }
+    foundUser = JSON.parse(foundUser)
+    return await new Promise(resolve => {
+      setTimeout(() => setUser(foundUser as unknown as User), 1000)
+      resolve(null)
+    })
+  }
+
   useEffect(() => {
-    if (!isLogged) navigate(routes.login)
+    asyncSetUser()
   }, [])
 
   const handleLogout = async () => {
     setIsLoading(true)
     await new Promise(resolve =>
       setTimeout(() => {
+        localStorage.removeItem('user')
         resolve(null)
-        setIsLogged(false)
       }, 2000)
     )
+    setUser(null)
+    setIsLogged(false)
     setIsLoading(false)
     navigate(routes.login)
+    return
   }
+
+  if (!user) return <p>Cargando...</p>
 
   return (
     <>
