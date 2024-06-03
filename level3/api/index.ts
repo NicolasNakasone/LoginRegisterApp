e8a8b4c55a0a60827bfd2c105517fcfd461aa512
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-03 00:08:28 -0300

    feat(file): added index.ts as entry point of the api

---
+++ level3/api/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import { configDotenv } from 'dotenv'
+
+configDotenv()
+
+const API_PORT = process.env.API_PORT || 4000
+
+const server = express()
+server.set('port', API_PORT)
+
+server.get('/', (req, res) => {
+  res.send('LoginRegister API')
+})
+
+server.listen(API_PORT, () => {
+  console.log(`Servidor escuchando en puerto ${API_PORT}`)
+})
