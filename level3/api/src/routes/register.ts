ce3b52d020db2ad3d8dd5fcb3d2c05efd648652e
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-05 13:22:45 -0300

    feat(router): added register router

---
+++ level3/api/src/routes/register.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { routes } from 'src/constants/routes'
+
+export const registerRouter = express.Router()
+
+registerRouter.get('/', (req, res) => {
+  res.send(`<h1>LoginRegisterApp - ${routes.register}<h1>`)
+})
+
+registerRouter.post('/', () => null)
