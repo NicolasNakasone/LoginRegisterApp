97ed3a3ac3b1889b1f482365bc9b56bf9eddec5a
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-05 13:17:33 -0300

    feat(middleware): added middleware on server.ts to fix CORS errors

--- level3/api/src/server.ts
+++ level3/api/src/server.ts
@@ -14,3 +14,11 @@ server.set('port', API_PORT || 4001)
 server.use('/', router)
 
 server.use(logger('dev'))
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
