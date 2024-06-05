d08ee49fadf76a034cc7f8e66487fe76eec9d504
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-05 13:21:50 -0300

    feat(router): added login router

---
+++ level3/api/src/routes/login.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import { routes } from 'src/constants/routes'
+
+export const loginRouter = express.Router()
+
+loginRouter.get('/', (req, res) => {
+  res.send(`<h1>LoginRegister API - ${routes.login}<h1>`)
+})
+
+loginRouter.post('/', (req, res, next) => {
+  try {
+    console.log(req)
+  } catch (error) {
+    next(error)
+  }
+})
