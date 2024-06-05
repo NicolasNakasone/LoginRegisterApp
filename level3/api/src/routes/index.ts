2d671d36d30b165fa5de2a346ec21c030062d3c0
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-05 13:15:07 -0300

    feat(router): created Express router

---
+++ level3/api/src/routes/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import { routes } from 'src/constants/routes'
+// import { loginRouter } from 'src/routes/login'
+// import { registerRouter } from 'src/routes/register'
+
+export const router = express.Router()
+
+// router.use(routes.login, loginRouter)
+// router.use(routes.register, registerRouter)
+
+router.get('/', (req, res) => {
+  res.send(`<h1>LoginRegister API<h1>`)
+})
