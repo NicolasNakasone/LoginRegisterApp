f62656c9d60ccfe1c7a5c77875f51d962e507d7d
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-04 23:54:30 -0300

    fix(disable): added eslint-disable to console.log on index.ts

--- level3/api/index.ts
+++ level3/api/index.ts
@@ -3,5 +3,6 @@ import { server } from 'src/server'
 const PORT = server.get('port')
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Servidor escuchando en puerto ${PORT}`)
 })
