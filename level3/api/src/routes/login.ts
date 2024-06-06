75729fd4c3d50452837360abd459b170522a0dbb
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-06 17:09:59 -0300

    refactor(function): replaced function defined on loginRouter with loginUser

--- level3/api/src/routes/login.ts
+++ level3/api/src/routes/login.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import { routes } from 'src/constants/routes'
-import { userList } from 'src/mocks/userList'
-import { Error } from 'src/types'
+import { loginUser } from 'src/controllers/login'
 
 export const loginRouter = express.Router()
 
@@ -9,32 +8,4 @@ loginRouter.get('/', (req, res) => {
   res.send(`<h1>LoginRegister API - ${routes.login}<h1>`)
 })
 
-loginRouter.post('/', (req, res, next) => {
-  try {
-    const { email, password } = req.body
-
-    const foundUser = userList[email]
-
-    if (!foundUser)
-      res.send({ code: 'USER_NOT_EXISTS', message: '❌ Usuario no registrado' } as Error)
-
-    const isSameEmail = foundUser.email === email
-    const isSamePassword = foundUser.password === password
-
-    /* No tiene sentido esto ya que para llegar aca tuvo que encontrar al user
-      por medio de la key que es siempre el email, y logicamente nunca se va a
-      guardar un email diferente al de la key. Esto tendria mas sentido en el
-      futuro cuando se guarde en una DB con el id como key (aunque quizas ya no
-      funcione asi { [key]: User })
-    */
-    if (!isSameEmail)
-      res.send({ code: 'EMAIL_NOT_MATCH', message: '❌ Correo incorrecto' } as Error)
-
-    if (!isSamePassword)
-      res.send({ code: 'PASSWORD_NOT_MATCH', message: '❌ Contraseña incorrecta' } as Error)
-
-    res.send(foundUser)
-  } catch (error) {
-    next(error)
-  }
-})
+loginRouter.post('/', loginUser)
