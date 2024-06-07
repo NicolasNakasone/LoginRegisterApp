45a18d7599cccaecf2e139d00d73fd0c738215c4
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-07 15:21:04 -0300

    feat(type): created PublicUser on client and commented unused functions on api.ts

--- level3/client/src/api.ts
+++ level3/client/src/api.ts
@@ -5,22 +5,25 @@ export interface User {
   full_name: string
 }
 
-export type UserInput = Omit<User, 'id'>
+export type PublicUser = Omit<User, 'password'>
 
-export type UserList = Record<string, User>
+export type UserInput = Omit<User, 'id'>
+export type LoginInput = Omit<User, 'id' | 'full_name'>
+
+// export type UserList = Record<string, User>
 
 const TIMEOUT_MS = 1000
 
 export const api = {
-  getUsers: (): Promise<UserList> => {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        const storedUsers = localStorage.getItem('users')
-        resolve(storedUsers ? JSON.parse(storedUsers) : {})
-      }, TIMEOUT_MS)
-    })
-  },
-  getUser: (): Promise<User> => {
+  // getUsers: (): Promise<UserList> => {
+  //   return new Promise(resolve => {
+  //     setTimeout(() => {
+  //       const storedUsers = localStorage.getItem('users')
+  //       resolve(storedUsers ? JSON.parse(storedUsers) : {})
+  //     }, TIMEOUT_MS)
+  //   })
+  // },
+  getUser: (): Promise<PublicUser> => {
     return new Promise(resolve => {
       setTimeout(() => {
         const foundUser = localStorage.getItem('user')
@@ -31,37 +34,37 @@ export const api = {
       }, TIMEOUT_MS)
     })
   },
-  addUser: (newUser: UserInput): Promise<User> => {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        const storedUsers = localStorage.getItem('users')
-        let userList: UserList = {}
+  // addUser: (newUser: UserInput): Promise<User> => {
+  //   return new Promise(resolve => {
+  //     setTimeout(() => {
+  //       const storedUsers = localStorage.getItem('users')
+  //       let userList: UserList = {}
 
-        if (storedUsers) userList = JSON.parse(storedUsers)
+  //       if (storedUsers) userList = JSON.parse(storedUsers)
 
-        userList[newUser.email] = { id: `${+new Date()}`, ...newUser }
-        localStorage.setItem('users', JSON.stringify(userList))
+  //       userList[newUser.email] = { id: `${+new Date()}`, ...newUser }
+  //       localStorage.setItem('users', JSON.stringify(userList))
 
-        resolve(userList[newUser.email])
-      }, TIMEOUT_MS)
-    })
-  },
-  findUser: (key: string): Promise<User | null> => {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        const storedUsers = localStorage.getItem('users')
-        resolve(storedUsers ? JSON.parse(storedUsers)[key] : storedUsers)
-      }, TIMEOUT_MS)
-    })
-  },
-  login: (newUser: User): Promise<User> => {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        localStorage.setItem('user', JSON.stringify(newUser))
-        resolve(newUser)
-      }, TIMEOUT_MS)
-    })
-  },
+  //       resolve(userList[newUser.email])
+  //     }, TIMEOUT_MS)
+  //   })
+  // },
+  // findUser: (key: string): Promise<User | null> => {
+  //   return new Promise(resolve => {
+  //     setTimeout(() => {
+  //       const storedUsers = localStorage.getItem('users')
+  //       resolve(storedUsers ? JSON.parse(storedUsers)[key] : storedUsers)
+  //     }, TIMEOUT_MS)
+  //   })
+  // },
+  // login: (newUser: User): Promise<User> => {
+  //   return new Promise(resolve => {
+  //     setTimeout(() => {
+  //       localStorage.setItem('user', JSON.stringify(newUser))
+  //       resolve(newUser)
+  //     }, TIMEOUT_MS)
+  //   })
+  // },
   logout: (): Promise<void> => {
     return new Promise(resolve =>
       setTimeout(() => {
