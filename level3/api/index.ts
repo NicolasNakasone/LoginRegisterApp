39ec0d88fcac53c8c5a2988a4fd6b43336a5ce82
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-07 22:06:59 -0300

    feat(sync): added database sync method before server initialization

--- level3/api/index.ts
+++ level3/api/index.ts
@@ -1,8 +1,15 @@
+/* eslint-disable no-console */
+import { database } from 'src/database'
 import { server } from 'src/server'
 
 const PORT = server.get('port')
 
-server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Servidor escuchando en puerto ${PORT}`)
-})
+database
+  .sync()
+  .then(() => {
+    console.log('Base de datos y tablas creadas')
+    server.listen(PORT, () => {
+      console.log(`Servidor escuchando en puerto ${PORT}`)
+    })
+  })
+  .catch(err => console.error('Error al sincronizar la base de datos:', err))
