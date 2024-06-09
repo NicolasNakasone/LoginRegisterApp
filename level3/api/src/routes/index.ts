a3e9b07008575b2916c89c2a0a280fb3bd5ed852
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-09 19:53:26 -0300

    feat(router): added usersRouter on server router

--- level3/api/src/routes/index.ts
+++ level3/api/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import { routes } from 'src/constants/routes'
 import { loginRouter } from 'src/routes/login'
 import { registerRouter } from 'src/routes/register'
+import { usersRouter } from 'src/routes/users'
 
 export const router = express.Router()
 
 router.use(routes.login, loginRouter)
 router.use(routes.register, registerRouter)
+router.use(routes.users, usersRouter)
 
 router.get('/', (req, res) => {
   res.send(`<h1>LoginRegister API<h1>`)
