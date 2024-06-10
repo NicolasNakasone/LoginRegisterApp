7bb1ab69c80e9cd8b9e6ee24b67e45378a26a619
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-10 17:20:06 -0300

    feat(router): added refreshTokenRouter to app router

--- level3/api/src/routes/index.ts
+++ level3/api/src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { routes } from 'src/constants/routes'
 import { loginRouter } from 'src/routes/login'
+import { refreshTokenRouter } from 'src/routes/refreshToken'
 import { registerRouter } from 'src/routes/register'
 import { usersRouter } from 'src/routes/users'
 
@@ -9,6 +10,7 @@ export const router = express.Router()
 router.use(routes.login, loginRouter)
 router.use(routes.register, registerRouter)
 router.use(routes.users, usersRouter)
+router.use(routes.refreshToken, refreshTokenRouter)
 
 router.get('/', (req, res) => {
   res.send(`<h1>LoginRegister API<h1>`)
