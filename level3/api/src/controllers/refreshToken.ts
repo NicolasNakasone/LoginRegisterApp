a177127466837fd8658df1d53dfd03f5b976acaf
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-11 17:00:54 -0300

    chore(comment): added comment to explain why status is not changed

--- level3/api/src/controllers/refreshToken.ts
+++ level3/api/src/controllers/refreshToken.ts
@@ -7,6 +7,10 @@ const { JWT_SECRET, JWT_REFRESH_SECRET } = process.env
 export const refreshToken: RequestHandler = (req, res, next) => {
   try {
     const { refreshToken } = req.body
+    /* Aca no es necesario porque en el front no hay validaciones
+      de status para esta request, si falla en el cliente
+      se devuelve el error, y se ejecuta el logout 
+    */
     if (!refreshToken) res.status(401).send({ message: 'Refresh token required' })
 
     jwt.verify(`${refreshToken}`, JWT_REFRESH_SECRET || '87654321', (error, decodedToken) => {
