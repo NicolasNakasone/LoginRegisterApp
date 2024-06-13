c7a5a0e01978bf3dbccae6a08cee6eaa500b0637
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-12 23:58:13 -0300

    feat(router): added new router to app router

--- level3/api/src/routes/index.ts
+++ level3/api/src/routes/index.ts
@@ -3,6 +3,7 @@ import { routes } from 'src/constants/routes'
 import { loginRouter } from 'src/routes/login'
 import { refreshTokenRouter } from 'src/routes/refreshToken'
 import { registerRouter } from 'src/routes/register'
+import { passwordRouter } from 'src/routes/resetPassword'
 import { usersRouter } from 'src/routes/users'
 
 export const router = express.Router()
@@ -11,6 +12,8 @@ router.use(routes.login, loginRouter)
 router.use(routes.register, registerRouter)
 router.use(routes.users, usersRouter)
 router.use(routes.refreshToken, refreshTokenRouter)
+// Va home, porque en el router hay handlers a diferentes rutas
+router.use(routes.home, passwordRouter)
 
 router.get('/', (req, res) => {
   res.send(`<h1>LoginRegister API<h1>`)
