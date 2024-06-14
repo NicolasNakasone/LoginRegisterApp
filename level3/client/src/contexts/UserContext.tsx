6699da24ed155015cbc82f0b0bd75ad5a04afa20
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-14 17:32:06 -0300

    fix(delete): deleted unused state on UserContext

--- level3/client/src/contexts/UserContext.tsx
+++ level3/client/src/contexts/UserContext.tsx
@@ -4,17 +4,13 @@ import { NavigateFunction } from 'react-router-dom'
 import { AuthenticatedUser } from 'src/api'
 
 interface UserContextProps {
-  isLogged: boolean
   user: AuthenticatedUser | null
   navigate: NavigateFunction
-  setIsLogged(value: SetStateAction<boolean>): void
   setUser(value: SetStateAction<AuthenticatedUser | null>): void
 }
 
 export const UserContext = createContext<UserContextProps>({
-  isLogged: false,
   user: null,
-  setIsLogged: () => null,
   setUser: () => null,
   navigate: () => null,
 })
--- level3/client/src/contexts/UserProvider.tsx
+++ level3/client/src/contexts/UserProvider.tsx
@@ -5,22 +5,15 @@ import { AuthenticatedUser } from 'src/api'
 import { UserContext } from 'src/contexts/UserContext'
 
 export const UserProvider = ({ children }: { children: JSX.Element }): JSX.Element => {
-  const [isLogged, setIsLogged] = useState(false)
-  /* TODO: Modificar este state, para que contenga isLogged dentro
-    y dentro de data el user. Esto para que mas adelante se pueda
-    agregar ahi el accessToken y lo que haga falta
-  */
   const [user, setUser] = useState<AuthenticatedUser | null>(null)
 
-  /* Agregar un contexto nuevo que sea algo como AuthProvider o algo asi,
+  /* TODO: Agregar un contexto nuevo que sea algo como AuthProvider o algo asi,
     Para agregar la proteccion/redireccion de rutas y eso, y asi mover
     esos controles de cada pagina
   */
   const navigate = useNavigate()
 
   return (
-    <UserContext.Provider value={{ isLogged, user, navigate, setIsLogged, setUser }}>
-      {children}
-    </UserContext.Provider>
+    <UserContext.Provider value={{ user, navigate, setUser }}>{children}</UserContext.Provider>
   )
 }
