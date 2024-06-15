1e00ab21c4389b7439ebfa7c819db2ecb81bb817
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-15 18:27:53 -0300

    feat(condition): added condition on loginUser controller if some credential does not exist

--- level3/api/src/controllers/login.ts
+++ level3/api/src/controllers/login.ts
@@ -8,6 +8,11 @@ export const loginUser: RequestHandler = async (req, res, next) => {
   try {
     const { email, password: plainTextPassword } = req.body
 
+    if (!email || !plainTextPassword)
+      return res.status(400).send({
+        message: '❌ Las credenciales son requeridas',
+      })
+
     const existingUser = await UserModel.findOne({ where: { email } })
 
     if (!existingUser) {
